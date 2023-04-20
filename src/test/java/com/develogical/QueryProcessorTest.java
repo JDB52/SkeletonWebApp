e651d1f3b7d7b99438367d3d756aac710da3b6be
jonathanbush <jonathanbushkd@gmail.com>
2023-04-20 13:08:14 +0100

    Setup for fly

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -17,7 +17,7 @@ public class QueryProcessorTest {
 
   @Test
   public void knowsAboutShakespeare() throws Exception {
-    assertThat(queryProcessor.process("Shakespeare"), containsString("author"));
+    assertThat(queryProcessor.process("Shakespeare"), containsString("playwright"));
   }
 
 //  @Test
